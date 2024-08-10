04586941295e3341437c374ac9fef9f7feefb6b7
alanzhang03 <alan.s.zhang@gmail.com>
2024-08-10 15:05:30 -0400

    mapping grid

--- components/ui/ProjectsCardDisplay.jsx
+++ components/ui/ProjectsCardDisplay.jsx
@@ -4,33 +4,53 @@ import "./styles/ProjectsCardDisplay.scss";
 const ProjectsCardDisplay = () => {
 	const projects = [
 		{
-			projectTitle: "Tic-Tac-Toe",
-			projectDescription:
+			Title: "Tic-Tac-Toe",
+			Description:
 				"Tic Tac Toe game played against either an AI (using minimax algorithm) or another player (human).",
-			projectLink: "https://alantictactoe.onrender.com/",
-			projectGitHubLink: "https://github.com/alanzhang03/tictactoe",
-			projectImage: "",
+			Link: "https://alantictactoe.onrender.com/",
+			GitHubLink: "https://github.com/alanzhang03/tictactoe",
+			Image: "",
 		},
 		{
-			projectTitle: "Apple website clone",
-			projectDescription: "Apple Iphone webpage clone",
+			Title: "Apple website clone",
+			Description: "Apple Iphone webpage clone",
 			projectLink: "https://apple-website-clone-46re.onrender.com/",
-			projectGitHubLink: "https://github.com/alanzhang03/apple_website",
-			projectImage: "",
+			GitHubLink: "https://github.com/alanzhang03/apple_website",
+			Image: "",
+		},
+		{
+			Title: "CV Builder",
+			Description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+			Link: "https://apple-website-clone-46re.onrender.com/",
+			GitHubLink: "https://github.com/alanzhang03/apple_website",
+			Image: "",
+		},
+		{
+			Title: "Duis eleifend odio id quam ultrices dignissim",
+			Description: "nteger eget urna interdum, hendrerit ligula in, dapibus do",
+			Link: "https://apple-website-clone-46re.onrender.com/",
+			GitHubLink: "https://github.com/alanzhang03/apple_website",
+			Image: "",
 		},
 	];
+
+	const projectDivs = projects.map((project, index) => {
+		return (
+			<div key={index}>
+				<div className="test-grid">
+					{project.Title}
+					{project.Description}
+					{project.Link}
+					{project.GitHubLink}
+				</div>
+			</div>
+		);
+	});
 
 	return (
 		<section className="projects-card-display">
 			<div>ProjectsCardDisplay</div>
-			<div className="projects-display-container">
-				<div className="test-grid"> test grid 1</div>
-				<div className="test-grid"> test grid 2</div>
-				<div className="test-grid"> test grid 3</div>
-				<div className="test-grid"> test grid 4 </div>
-				<div className="test-grid"> test grid 4 </div>
-				<div className="test-grid"> test grid 4 </div>
-			</div>
+			<div className="projects-display-container">{projectDivs}</div>
 		</section>
 	);
 };
