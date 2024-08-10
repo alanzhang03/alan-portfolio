91479137898f58cc311c00e6a5d81e8889c7b134
alanzhang03 <alan.s.zhang@gmail.com>
2024-08-10 19:07:03 -0400

    projects grid setup

--- components/ui/ProjectsCardDisplay.jsx
+++ components/ui/ProjectsCardDisplay.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import "./styles/ProjectsCardDisplay.scss";
+import Image from "next/image";
+import TicTacToeImg from "../../public/Assets/TicTacToe.png";
 
 const ProjectsCardDisplay = () => {
 	const projects = [
@@ -9,7 +11,12 @@ const ProjectsCardDisplay = () => {
 				"Tic Tac Toe game played against either an AI (using minimax algorithm) or another player (human).",
 			Link: "https://alantictactoe.onrender.com/",
 			GitHubLink: "https://github.com/alanzhang03/tictactoe",
-			Image: "",
+			Image: TicTacToeImg,
+			tech1: "",
+			tech2: "",
+			tech3: "",
+			tech4: "",
+			tech5: "",
 		},
 		{
 			Title: "Apple website clone",
@@ -17,6 +24,11 @@ const ProjectsCardDisplay = () => {
 			projectLink: "https://apple-website-clone-46re.onrender.com/",
 			GitHubLink: "https://github.com/alanzhang03/apple_website",
 			Image: "",
+			tech1: "",
+			tech2: "",
+			tech3: "",
+			tech4: "",
+			tech5: "",
 		},
 		{
 			Title: "CV Builder",
@@ -24,6 +36,11 @@ const ProjectsCardDisplay = () => {
 			Link: "https://apple-website-clone-46re.onrender.com/",
 			GitHubLink: "https://github.com/alanzhang03/apple_website",
 			Image: "",
+			tech1: "",
+			tech2: "",
+			tech3: "",
+			tech4: "",
+			tech5: "",
 		},
 		{
 			Title: "Duis eleifend odio id quam ultrices dignissim",
@@ -31,17 +48,41 @@ const ProjectsCardDisplay = () => {
 			Link: "https://apple-website-clone-46re.onrender.com/",
 			GitHubLink: "https://github.com/alanzhang03/apple_website",
 			Image: "",
+			tech1: "",
+			tech2: "",
+			tech3: "",
+			tech4: "",
+			tech5: "",
 		},
 	];
 
 	const projectDivs = projects.map((project, index) => {
 		return (
 			<div key={index}>
-				<div className="test-grid">
-					{project.Title}
-					{project.Description}
+				<div className="project-grid">
+					<Image
+						className="projects-images"
+						src={project.Image}
+						alt="Picture of projects"
+					></Image>
+					<h1 className="project-title">{project.Title}</h1>
+					<p className="project-description">{project.Description}</p>
+					<div className="project-tech-link-container">
+						<div className="tech-container">
+							{project.tech1}
+							{project.tech2}
+							{project.tech3}
+							{project.tech4}
+							{project.tech5}
+
+						</div>
+						<p className="project-nav">Check Live Site</p>
+					</div>
+
+					{/*
 					{project.Link}
-					{project.GitHubLink}
+                    {project.GitHubLink}
+					*/}
 				</div>
 			</div>
 		);
@@ -50,6 +91,7 @@ const ProjectsCardDisplay = () => {
 	return (
 		<section className="projects-card-display">
 			<div>ProjectsCardDisplay</div>
+
 			<div className="projects-display-container">{projectDivs}</div>
 		</section>
 	);
