46c7a2ad005c73113e4f0fbcb531a3aea50d3698
alanzhang03 <alan.s.zhang@gmail.com>
2024-08-11 17:16:31 -0400

    project

--- components/ui/ProjectsCardDisplay.jsx
+++ components/ui/ProjectsCardDisplay.jsx
@@ -3,17 +3,40 @@
 import React from "react";
 import "./styles/ProjectsCardDisplay.scss";
 import Image from "next/image";
-import TicTacToeImg from "../../public/Assets/TicTacToe.png";
 import { FaLocationArrow } from "react-icons/fa6";
+
+//Project Pictures
+import TicTacToeImg from "../../public/Assets/TicTacToe.png";
+import PreviousPort from "../../public/Assets/previousPort.png";
+import CvApp from "../../public/Assets/CvApp.png";
+import AppleClone from "../../public/Assets/AppleClone.png";
+
+//Tech Stack Logos
 import ReactLogo from "../../public/Assets/reactLogo.svg";
 import JsLogo from "../../public/Assets/JsLogo.svg";
 import ThreeLogo from "../../public/Assets/threeLogo.svg";
 import GsapLogo from "../../public/Assets/gsapLogo.svg";
 import NextLogo from "../../public/Assets/nextLogo.svg";
 import SassLogo from "../../public/Assets/sassLogo.svg";
+import NodeLogo from "../../public/Assets/nodejsLogo.svg";
+import CssLogo from "../../public/Assets/CssLogo.svg";
+import HtmlLogo from "../../public/Assets/HtmlLogo.svg";
+import TailLogo from "../../public/Assets/tailLogo.svg";
 
 const ProjectsCardDisplay = () => {
 	const projects = [
+		{
+			Title: "Previous Web Portfolio",
+			Description: "My previous web portfolio",
+			Link: "https://alanzhang.onrender.com",
+			GitHubLink: "https://github.com/alanzhang03/apple_website",
+			Image: PreviousPort,
+			tech1: ReactLogo,
+			tech2: NodeLogo,
+			tech3: JsLogo,
+			tech4: CssLogo,
+			tech5: HtmlLogo,
+		},
 		{
 			Title: "Tic-Tac-Toe",
 			Description:
@@ -23,45 +46,34 @@ const ProjectsCardDisplay = () => {
 			Image: TicTacToeImg,
 			tech1: ReactLogo,
 			tech2: JsLogo,
-			tech3: GsapLogo,
-			tech4: NextLogo,
-			tech5: SassLogo,
+			tech3: HtmlLogo,
+			tech4: CssLogo,
+			tech5: "",
 		},
 		{
 			Title: "Apple website clone",
 			Description: "Apple Iphone webpage clone",
 			Link: "https://apple-website-clone-46re.onrender.com/",
 			GitHubLink: "https://github.com/alanzhang03/apple_website",
-			Image: "",
-			tech1: ReactLogo,
-			tech2: ThreeLogo,
+			Image: AppleClone,
+			tech1: NextLogo,
+			tech2: GsapLogo,
 			tech3: JsLogo,
-			tech4: GsapLogo,
-			tech5: NextLogo,
+			tech4: TailLogo,
+			tech5: ThreeLogo,
 		},
 		{
 			Title: "CV Builder",
-			Description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-			Link: "https://apple-website-clone-46re.onrender.com/",
-			GitHubLink: "https://github.com/alanzhang03/apple_website",
-			Image: "",
-			tech1: "",
-			tech2: "",
-			tech3: "",
-			tech4: "",
-			tech5: "",
-		},
-		{
-			Title: "Duis eleifend odio id quam ultrices dignissim",
-			Description: "nteger eget urna interdum, hendrerit ligula in, dapibus do",
-			Link: "https://apple-website-clone-46re.onrender.com/",
-			GitHubLink: "https://github.com/alanzhang03/apple_website",
-			Image: "",
-			tech1: "",
-			tech2: "",
-			tech3: "",
-			tech4: "",
-			tech5: "",
+			Description:
+				"A powerful CV builder designed specifically for software developers",
+			Link: "https://resume-cv-app.onrender.com/",
+			GitHubLink: "https://github.com/alanzhang03/Resume-CV-App",
+			Image: CvApp,
+			tech1: ReactLogo,
+			tech2: NodeLogo,
+			tech3: JsLogo,
+			tech4: CssLogo,
+			tech5: HtmlLogo,
 		},
 	];
 
@@ -69,15 +81,18 @@ const ProjectsCardDisplay = () => {
 		return (
 			<div key={index}>
 				<div className="project-grid">
-					{project.Image && (
+					<div className="project-image-container">
 						<Image
 							className="projects-images"
 							src={project.Image}
 							alt={`${project.Title} image`}
 						/>
-					)}
-					<h1 className="project-title">{project.Title}</h1>
-					<p className="project-description">{project.Description}</p>
+					</div>
+					<div className="project-text-container">
+						{" "}
+						<h1 className="project-title">{project.Title}</h1>
+						<p className="project-description">{project.Description}</p>
+					</div>
 					<div className="project-tech-link-container">
 						<div className="tech-container">
 							<Image
@@ -107,7 +122,9 @@ const ProjectsCardDisplay = () => {
 							/>
 						</div>
 						<p className="project-nav">
-							Check Live Site <FaLocationArrow className="location-arrow" />
+							<a target="_blank" href={project.Link}>
+								Check Live Site <FaLocationArrow className="location-arrow" />
+							</a>
 						</p>
 					</div>
 				</div>
