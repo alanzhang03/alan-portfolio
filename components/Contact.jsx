587198552d6e909add8611f2508b5d3b9364ff1d
alanzhang03 <alan.s.zhang@gmail.com>
2024-08-18 15:41:12 -0400

    installed email

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import "../styles/Contact.scss";
 import ContactForm from "./ui/ContactForm";
+import Email from "../emails/Email";
 
 const Contact = () => {
 	return (
 		<section id="contact" className="main-contact">
 			<h1 className="contact-heading">Contact Me</h1>
 			<ContactForm />
+			<Email />
 		</section>
 	);
 };
---
+++ emails/Email.jsx
@@ -0,0 +1,18 @@
+"use client";
+
+import "./Email.scss";
+import { Button, Html } from "@react-email/components";
+import * as React from "react";
+
+export default function Emails() {
+	return (
+		<section className="email-main">
+			<Button
+				href="https://example.com"
+				style={{ background: "#000", color: "#fff", padding: "12px 20px" }}
+			>
+				Click me
+			</Button>
+		</section>
+	);
+}
