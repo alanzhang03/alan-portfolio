85053fb17b9966f5001118fa0e964cb217198780
alanzhang03 <alan.s.zhang@gmail.com>
2024-08-18 20:11:58 -0400

    contact form

--- components/ui/ContactForm.jsx
+++ components/ui/ContactForm.jsx
@@ -1,7 +1,35 @@
 import React from "react";
+import "./styles/ContactForm.scss";
 
-const ContactForm = () => {
-	return <div>ContactForm</div>;
+const ContactForm = ({ onInputChange }) => {
+	return (
+		<form className="contact-form-container">
+			<span>Name</span>
+			<input
+				type="text"
+				name="name"
+				placeholder="Your Name"
+				onChange={onInputChange}
+				required
+			/>
+			<span>Email</span>
+			<input
+				type="email"
+				name="email"
+				placeholder="Your Email"
+				onChange={onInputChange}
+				required
+			/>
+
+			<span>Message</span>
+			<textarea
+				name="message"
+				placeholder="Your Message"
+				onChange={onInputChange}
+				required
+			></textarea>
+		</form>
+	);
 };
 
 export default ContactForm;
