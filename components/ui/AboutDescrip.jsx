271b358e995a7f74fc34eb971a6a8da4db5b1d31
alanzhang03 <alan.s.zhang@gmail.com>
2024-08-21 11:52:03 -0400

    changed font size cv

--- components/ui/AboutDescrip.jsx
+++ components/ui/AboutDescrip.jsx
@@ -11,92 +11,92 @@ import { MdOutlineFileDownload } from "react-icons/md";
 gsap.registerPlugin(ScrollTrigger);
 
 const AboutDescrip = () => {
-  useGSAP(() => {
-    gsap.to("#gsap-about-me-description span", {
-      scrollTrigger: ".about-me-description",
-      duration: 1.5,
-      delay: 0.5,
-      opacity: 1,
-      stagger: {
-        each: 0.165,
-        from: "start",
-      },
-    });
+	useGSAP(() => {
+		gsap.to("#gsap-about-me-description span", {
+			scrollTrigger: ".about-me-description",
+			duration: 1.5,
+			delay: 0.5,
+			opacity: 1,
+			stagger: {
+				each: 0.165,
+				from: "start",
+			},
+		});
 
-    gsap.to(".section-2-divider span", {
-      scrollTrigger: ".section-2-divider",
-      duration: 2,
-      delay: 2.5,
-      opacity: 1,
-      stagger: {
-        each: 0.175,
-        from: "start",
-      },
-    });
-    gsap.to(".download-cv-button", {
-      scrollTrigger: ".download-cv-button",
-      duration: 1.5,
-      delay: 3.5,
-      opacity: 1,
-    });
-  }, []);
+		gsap.to(".section-2-divider span", {
+			scrollTrigger: ".section-2-divider",
+			duration: 2,
+			delay: 2.5,
+			opacity: 1,
+			stagger: {
+				each: 0.175,
+				from: "start",
+			},
+		});
+		gsap.to(".download-cv-button", {
+			scrollTrigger: ".download-cv-button",
+			duration: 1.5,
+			delay: 3.5,
+			opacity: 1,
+		});
+	}, []);
 
-  return (
-    <>
-      <p id="gsap-about-me-description" className="about-me-description">
-        <span>Aspiring</span>
-        <span className="purple-text"> Full-Stack developer </span>
-        <span>and current undergrad student majoring in</span>
-        <span className="purple-text">
-          {" "}
-          <a target="_blank" href="https://www.cs.umd.edu/">
-            {" "}
-            Computer Science{" "}
-          </a>{" "}
-        </span>{" "}
-        <span>at the</span>
-        <span className="purple-text">
-          {" "}
-          <a target="_blank" href="https://www.cs.umd.edu/">
-            {" "}
-            University of Maryland, College Park{" "}
-          </a>{" "}
-        </span>{" "}
-        <span>(Expected grad in May 2025). </span>
-        <span>I have developed a wide range of</span>
-        <span className="purple-text">
-          <Link href="#projects"> interactive web applications </Link>
-        </span>
-        <span>which you can view in the</span>
-        <span className="purple-text">
-          <Link href="#projects"> projects section </Link>
-        </span>
-        <span>of this portfolio!</span>
-      </p>
+	return (
+		<>
+			<p id="gsap-about-me-description" className="about-me-description">
+				<span>Aspiring</span>
+				<span className="purple-text"> Full-Stack developer </span>
+				<span>and current undergrad student majoring in</span>
+				<span className="purple-text">
+					{" "}
+					<a target="_blank" href="https://www.cs.umd.edu/">
+						{" "}
+						Computer Science{" "}
+					</a>{" "}
+				</span>{" "}
+				<span>at the</span>
+				<span className="purple-text">
+					{" "}
+					<a target="_blank" href="https://www.cs.umd.edu/">
+						{" "}
+						University of Maryland, College Park{" "}
+					</a>{" "}
+				</span>{" "}
+				<span>(Expected grad in May 2025). </span>
+				<span>I have developed a wide range of</span>
+				<span className="purple-text">
+					<Link href="#projects"> interactive web applications </Link>
+				</span>
+				<span>which you can view in the</span>
+				<span className="purple-text">
+					<Link href="#projects"> projects section </Link>
+				</span>
+				<span>of this portfolio!</span>
+			</p>
 
-      <div className="about-section-2">
-        <div className="section-2-divider">
-          <span className="number">3</span>+ Years Of Experience
-        </div>
-        <div className="section-2-divider">
-          <span className="number">15</span>+ Completed Projects
-        </div>
-        <div className="section-2-divider">
-          <span className="number">20</span> + Technologies learned
-        </div>
-        <div className="section-2-divider">
-          <span className="number">800</span>+ Code <br></br>Commits
-        </div>
-      </div>
-      <div className="download-cv-container">
-        <a href="/AlanZhangResume.pdf" download>
-          <button className="download-cv-button">
-            Resume <MdOutlineFileDownload className="download-icon" />
-          </button>
-        </a>
-      </div>
-    </>
-  );
+			<div className="about-section-2">
+				<div className="section-2-divider">
+					<span className="number">3</span>+ Years Of Experience
+				</div>
+				<div className="section-2-divider">
+					<span className="number">15</span>+ Completed Projects
+				</div>
+				<div className="section-2-divider">
+					<span className="number">20</span> + Technologies learned
+				</div>
+				<div className="section-2-divider">
+					<span className="number">800</span>+ Code <br></br>Commits
+				</div>
+			</div>
+			<div className="download-cv-container">
+				<a href="/AlanZhangResume.pdf" download>
+					<button className="download-cv-button">
+						Download CV <MdOutlineFileDownload className="download-icon" />
+					</button>
+				</a>
+			</div>
+		</>
+	);
 };
 
 export default AboutDescrip;
