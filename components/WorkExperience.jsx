37d3734cec61e61da14931327e97e60e375b2630
alanzhang03 <107202923+alanzhang03@users.noreply.github.com>
2024-09-12 16:33:31 -0400

    work experience

--- components/WorkExperience.jsx
+++ components/WorkExperience.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import WorkUi from "./ui/WorkUi";
+import "../styles/WorkExperience.scss";
 
 const WorkExperience = () => {
-	return (
-		<div>
-			<WorkUi />
-		</div>
-	);
+  return (
+    <div>
+      <h1 className="work-experience-heading">
+        Work <span style={{ color: "#a371f7" }}>Experience</span>
+      </h1>
+      <WorkUi />
+    </div>
+  );
 };
 
 export default WorkExperience;
--- components/ui/WorkUi.jsx
+++ components/ui/WorkUi.jsx
@@ -1,16 +1,33 @@
 import React from "react";
 import "../ui/styles/WorkUi.scss";
 
-
 const WorkUi = () => {
+  const workExperience = [
+    {
+      companyName: "Mindhome",
+      companyLogo: "",
+      startDate: "",
+      endDate: "",
+      companyDescription: "",
+    },
+  ];
+
   return (
     <div className="work-ui-container">
-          
-          
-          
-        
+      {workExperience.map((company) => {
+        return (
+          <>
+            <div>{company.companyName}</div>
+            <div className="company-start-end-date-container">
+              <div>{company.startDate}</div>
+              <div>{company.endDate}</div>
+            </div>
+            <div>{company.companyDescription}</div>
+          </>
+        );
+      })}
     </div>
-  )
-}
+  );
+};
 
-export default WorkUi
+export default WorkUi;
