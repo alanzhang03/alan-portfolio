c15fd7f8414c5c314924e246a78dbb55390fa0ea
alanzhang03 <alan.s.zhang@gmail.com>
2024-09-12 22:43:18 -0400

    work experience addition

--- components/WorkExperience.jsx
+++ components/WorkExperience.jsx
@@ -3,14 +3,14 @@ import WorkUi from "./ui/WorkUi";
 import "../styles/WorkExperience.scss";
 
 const WorkExperience = () => {
-  return (
-    <div>
-      <h1 className="work-experience-heading">
-        Work <span style={{ color: "#a371f7" }}>Experience</span>
-      </h1>
-      <WorkUi />
-    </div>
-  );
+	return (
+		<div>
+			<h1 className="work-experience-heading">
+				Work <span style={{ color: "#a371f7" }}>Experience</span>
+			</h1>
+			<WorkUi className="work-experience-test" />
+		</div>
+	);
 };
 
 export default WorkExperience;
--- components/ui/WorkUi.jsx
+++ components/ui/WorkUi.jsx
@@ -2,33 +2,66 @@ import React from "react";
 import "../ui/styles/WorkUi.scss";
 
 const WorkUi = () => {
-  const workExperience = [
-    {
-      companyName: "Mindhome",
-      companyLogo: "",
-      startDate: "",
-      endDate: "",
-      companyDescription: "",
-    },
-  ];
+	const workExperience = [
+		{
+			companyName: "Mindhome",
+			companyLogo: "",
+			companyPosition: "Software Engineering Intern",
+			startDate: "April 2024",
+			endDate: "August 2024",
+			jobDescription: [
+				"• Worked on updates and enhancements to the company website, leveraging React for web application portions and React Native for mobile optimizations.",
+				"• Contributed personal insights and recommendations for leveraging React to enhance aesthetics and functionality of our web application.",
+				"• Collaborated closely with a team of interns to develop and refine React application projects, enhancing project outcomes through effective teamwork.",
+				"• Gained foundational skills in robotics programming by engaging with Linux and Python, gaining practical experience in developing and testing automated solutions.",
+			],
+		},
+		{
+			companyName: "Temple University",
+			companyLogo: "",
+			companyPosition: "Research Assistant Intern",
+			startDate: "August 2024",
+			endDate: "Now",
+			jobDescription: [
+				"• Worked on updates and enhancements to the company website, leveraging React for web application portions and React Native for mobile optimizations.",
+				"• Contributed personal insights and recommendations for leveraging React to enhance aesthetics and functionality of our web application.",
+				"• Collaborated closely with a team of interns to develop and refine React application projects, enhancing project outcomes through effective teamwork.",
+				"• Gained foundational skills in robotics programming by engaging with Linux and Python, gaining practical experience in developing and testing automated solutions.",
+			],
+		},
+	];
 
-  const workDiv = workExperience.map((company, index) =>)
-  return (
-    <div className="work-ui-container">
-      {workExperience.map((company) => {
-        return (
-          <>
-            <div>{company.companyName}</div>
-            <div className="company-start-end-date-container">
-              <div>{company.startDate}</div>
-              <div>{company.endDate}</div>
-            </div>
-            <div>{company.companyDescription}</div>
-          </>
-        );
-      })}
-    </div>
-  );
+	const workDiv = workExperience.map((company, index) => {
+		return (
+			<div className="work-ui-container" key={index}>
+				<h1 className="work-company-name">{company.companyName}</h1>
+				<p className="company-position">{company.companyPosition}</p>
+				<div className="company-start-end-date-container">
+					<p>
+						{company.startDate} -{" "}
+						<span className={company.endDate === "Now" ? "now-color" : ""}>
+							{company.endDate}
+						</span>
+					</p>
+				</div>
+				<div className="job-description-gap">
+					<ul className="job-description">
+						{company.jobDescription.map((desc, i) => (
+							<p key={i}>{desc}</p>
+						))}
+					</ul>
+				</div>
+			</div>
+		);
+	});
+
+	return (
+		<section className="experience-display">
+			<div className="work-ui-box">
+				<div>{workDiv}</div>
+			</div>
+		</section>
+	);
 };
 
 export default WorkUi;
