302e79a8dd1487ad42d9b5b26e54034592579fff
alanzhang03 <107202923+alanzhang03@users.noreply.github.com>
2025-02-24 15:17:37 -0500

    twitter link

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -7,58 +7,58 @@ import { FaFacebook } from "react-icons/fa6";
 import { SiInstagram } from "react-icons/si";
 
 const Footer = () => {
-	return (
-		<footer className="footer-container">
-			<div className="footer-content-container">
-				<div className="copyright-section">
-					<p>
-						&copy; 2024 <span>azhang03</span>. 
-					</p>
-				</div>
-				<div className="social-icons-container">
-					<a
-						href="https://www.linkedin.com/in/alan-zhang-a254b8233/"
-						target="_blank"
-						rel="noopener noreferrer"
-					>
-						<TiSocialLinkedinCircular className="linkedin-icon" />
-					</a>
+  return (
+    <footer className="footer-container">
+      <div className="footer-content-container">
+        <div className="copyright-section">
+          <p>
+            &copy; 2024 <span>azhang03</span>.
+          </p>
+        </div>
+        <div className="social-icons-container">
+          <a
+            href="https://www.linkedin.com/in/alan-zhang-a254b8233/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <TiSocialLinkedinCircular className="linkedin-icon" />
+          </a>
 
-					<a
-						href="https://github.com/alanzhang03"
-						target="_blank"
-						rel="noopener noreferrer"
-					>
-						<IoLogoGithub className="github-icon" />
-					</a>
+          <a
+            href="https://github.com/alanzhang03"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <IoLogoGithub className="github-icon" />
+          </a>
 
-					<a
-						href="https://twitter.com/cloudeelol"
-						target="_blank"
-						rel="noopener noreferrer"
-					>
-						<AiFillTwitterCircle className="twitter-icon" />
-					</a>
+          <a
+            href="https://twitter.com/alanzhang003"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <AiFillTwitterCircle className="twitter-icon" />
+          </a>
 
-					<a
-						href="https://www.facebook.com/profile.php?id=100009265073408"
-						target="_blank"
-						rel="noopener noreferrer"
-					>
-						<FaFacebook className="facebook-icon" />
-					</a>
+          <a
+            href="https://www.facebook.com/profile.php?id=100009265073408"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FaFacebook className="facebook-icon" />
+          </a>
 
-					<a
-						href="https://www.instagram.com/_alanzhang_3/"
-						target="_blank"
-						rel="noopener noreferrer"
-					>
-						<SiInstagram className="insta-icon" />
-					</a>
-				</div>
-			</div>
-		</footer>
-	);
+          <a
+            href="https://www.instagram.com/_alanzhang_3/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <SiInstagram className="insta-icon" />
+          </a>
+        </div>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
