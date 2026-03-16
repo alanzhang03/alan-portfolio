1a2a9a69bfd8f8865e4b837ee710614b897766d6
alanzhang03 <107202923+alanzhang03@users.noreply.github.com>
2026-03-16 19:58:28 -0400

    adjusted order

--- components/HomePage.jsx
+++ components/HomePage.jsx
@@ -1,54 +1,54 @@
-"use client";
+'use client';
 
-import React from "react";
-import gsap from "gsap";
-import { useGSAP } from "@gsap/react";
-import "../styles/HomePage.scss";
-import { FaArrowDownLong } from "react-icons/fa6";
-import TextLoop from "./ui/TextLoop";
-import Image from "next/image";
+import React from 'react';
+import gsap from 'gsap';
+import { useGSAP } from '@gsap/react';
+import '../styles/HomePage.scss';
+import { FaArrowDownLong } from 'react-icons/fa6';
+import TextLoop from './ui/TextLoop';
+import Image from 'next/image';
 
-import { FaGithub } from "react-icons/fa";
-import { CiLinkedin } from "react-icons/ci";
-import { MdOutlineEmail } from "react-icons/md";
-import { CiTwitter } from "react-icons/ci";
-import { FaInstagram } from "react-icons/fa6";
+import { FaGithub } from 'react-icons/fa';
+import { CiLinkedin } from 'react-icons/ci';
+import { MdOutlineEmail } from 'react-icons/md';
+import { CiTwitter } from 'react-icons/ci';
+import { FaInstagram } from 'react-icons/fa6';
 
 const HomePage = () => {
   useGSAP(() => {
-    gsap.from(".hero-greeting", {
+    gsap.from('.hero-greeting', {
       delay: 0.3,
       duration: 0.6,
       y: 20,
       opacity: 0,
-      ease: "power2.out",
+      ease: 'power2.out',
     });
-    gsap.from(".hero-name", {
+    gsap.from('.hero-name', {
       delay: 0.5,
       duration: 0.7,
       y: 30,
       opacity: 0,
-      ease: "power2.out",
+      ease: 'power2.out',
     });
-    gsap.from(".hero-textloop", {
+    gsap.from('.hero-textloop', {
       delay: 0.8,
       duration: 0.6,
       y: 20,
       opacity: 0,
-      ease: "power2.out",
+      ease: 'power2.out',
     });
-    gsap.to(".social-box", {
+    gsap.to('.social-box', {
       delay: 1.1,
       opacity: 1,
       duration: 0.5,
-      stagger: { each: 0.1, from: "start" },
+      stagger: { each: 0.1, from: 'start' },
     });
-    gsap.to(".profile-photo", {
+    gsap.to('.profile-photo', {
       opacity: 1,
       delay: 0.4,
       duration: 1.2,
     });
-    gsap.from("#gsap-arrow-down", {
+    gsap.from('#gsap-arrow-down', {
       opacity: 0,
       delay: 1.8,
       duration: 1,
@@ -56,48 +56,75 @@ const HomePage = () => {
   }, []);
 
   const socialLinks = [
-    { Index: "1", Title: "Twitter", Icon: <CiTwitter />, Link: "https://twitter.com/alanzhang003" },
-    { Index: "2", Title: "LinkedIn", Icon: <CiLinkedin />, Link: "https://www.linkedin.com/in/alan-zhang-a254b8233/" },
-    { Index: "3", Title: "GitHub", Icon: <FaGithub />, Link: "https://github.com/alanzhang03" },
-    { Index: "4", Title: "Email", Icon: <MdOutlineEmail />, Link: "mailto:alan.s.zhang04@gmail.com" },
-    { Index: "5", Title: "Instagram", Icon: <FaInstagram />, Link: "https://www.instagram.com/_alanzhang_3/" },
+    {
+      Index: '1',
+      Title: 'LinkedIn',
+      Icon: <CiLinkedin />,
+      Link: 'https://www.linkedin.com/in/alan-zhang-a254b8233/',
+    },
+    {
+      Index: '2',
+      Title: 'GitHub',
+      Icon: <FaGithub />,
+      Link: 'https://github.com/alanzhang03',
+    },
+    {
+      Index: '3',
+      Title: 'Email',
+      Icon: <MdOutlineEmail />,
+      Link: 'mailto:alan.s.zhang04@gmail.com',
+    },
+    {
+      Index: '4',
+      Title: 'Twitter',
+      Icon: <CiTwitter />,
+      Link: 'https://twitter.com/alanzhang003',
+    },
+    {
+      Index: '5',
+      Title: 'Instagram',
+      Icon: <FaInstagram />,
+      Link: 'https://www.instagram.com/_alanzhang_3/',
+    },
   ];
 
   const mapSocialLinks = socialLinks.map((socials) => (
-    <a key={socials.Index} target="_blank" href={socials.Link} rel="noreferrer">
-      <div className="social-box">
-        <div className="home-page-icon">{socials.Icon}</div>
-        <p className="socials-text">{socials.Title}</p>
+    <a key={socials.Index} target='_blank' href={socials.Link} rel='noreferrer'>
+      <div className='social-box'>
+        <div className='home-page-icon'>{socials.Icon}</div>
+        <p className='socials-text'>{socials.Title}</p>
       </div>
     </a>
   ));
 
   return (
-    <section id="homePage" className="main-homepage">
-      <div className="hero-content">
-        <div className="hero-left">
-          <p className="hero-greeting">Hi, I&apos;m</p>
-          <h1 className="hero-name">Alan Zhang</h1>
-          <div className="hero-textloop">
+    <section id='homePage' className='main-homepage'>
+      <div className='hero-content'>
+        <div className='hero-left'>
+          <p className='hero-greeting'>Hi, I&apos;m</p>
+          <h1 className='hero-name'>Alan Zhang</h1>
+          <div className='hero-textloop'>
             <TextLoop />
           </div>
-          <div className="socials-container">{mapSocialLinks}</div>
+          <div className='socials-container'>{mapSocialLinks}</div>
         </div>
-        <div className="hero-right">
-          <div className="profile-photo-container">
+        <div className='hero-right'>
+          <div className='profile-photo-container'>
             <Image
-              src="/Assets/alan-pic.JPG"
-              alt="Alan Zhang"
+              src='/Assets/alan-pic.JPG'
+              alt='Alan Zhang'
               width={320}
               height={320}
-              className="profile-photo"
+              className='profile-photo'
               priority
-              onClick={() => window.open("https://www.linkedin.com/in/azhang03/", "_blank")}
+              onClick={() =>
+                window.open('https://www.linkedin.com/in/azhang03/', '_blank')
+              }
             />
           </div>
         </div>
       </div>
-      <FaArrowDownLong id="gsap-arrow-down" className="arrow-down" />
+      <FaArrowDownLong id='gsap-arrow-down' className='arrow-down' />
     </section>
   );
 };
